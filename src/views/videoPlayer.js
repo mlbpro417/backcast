f8f98f406481f5b21fd506a25b809d77d85496bb
mlbpro417 <jtglandorf@gmail.com>
2018-01-30 21:19:58 -0800

    updating videoPlayer with attempt at accounting for edge case when models array is empty - i.e. something to show when there is no video to render

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,21 +1,29 @@
 var VideoPlayerView = Backbone.View.extend({
-
+  
   initialize: function() {
     this.collection.on('select', this.renderSelected, this);
-    // console.log(this);
   },
 
   render: function() {
     // this render sets the initial render and is given the first model instance in the collection
+
+    // Attempt to write for edge case when models array is empty
+    // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // if (!this.collection.models) {
+    //   this.template = templateURL('src/templates/videoPlayerV2.html');
+    //   this.$el.html(this.template());
+    //   return this;
+    // } else {
+    // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
     this.$el.html(this.template(this.collection.models[0].attributes));
     return this;
+    // }
   },
 
   renderSelected: function(video) {
     // this re-renders the player when a 'select' event is found
-    this.$el.html(this.template(video.attributes));
-    return this;
-    console.log(video.attributes);
+    this.collection.models.unshift(video);
+    this.render();
   },
 
   template: templateURL('src/templates/videoPlayer.html')
